2544535ec27505e95e823202f68a528c986f141d
nemanja-z <nemanja14zivanovic@gmail.com>
2020-04-23 18:48:21 +0200

    production

---
+++ part3/phonebook-backend/build/precache-manifest.8a48ee2cf18e2f3b0563738610950240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302f19c4eec28f0aa446d688e2e6b07b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "083f14298fa4097b84cc",
+    "url": "/static/css/main.6e634280.chunk.css"
+  },
+  {
+    "revision": "ecfff7d17cb97d44e7b1",
+    "url": "/static/js/2.f446d7b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f446d7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083f14298fa4097b84cc",
+    "url": "/static/js/main.21b114b1.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
+app.use(express.static('build'))
 cors = require('cors')
 app.use(cors())
 app.use(express.json())
